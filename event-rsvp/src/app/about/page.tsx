3c7aedc8d4b2c215b2d1847b16cc9cc7d24596c0
gideonashiru <gideonashiru@gmail.com>
2025-04-19 01:37:13 -0500

    added a nav bar header to layout, will design home page next

---
+++ event-rsvp/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+
+const NavbarDemoPage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold my-8">
+        About Page
+      </h1>
+ 
+    </div>
+  );
+};
+
+export default NavbarDemoPage;
